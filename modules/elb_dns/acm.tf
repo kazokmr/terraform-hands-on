7e9cd419dac6a12a4af565e91c0956ff3f94d6b8
Kazuhiko Okumura <engkokmr@gmail.com>
2021-11-07 15:36:36 +0900

    Chapter8 ロードバランサーとDNS
    SSLサーバー証明書を発行する
    - ACM(AWS Certificate Manager)を利用する
    - ワイルドカード証明書で発行する

---
+++ modules/elb_dns/acm.tf
@@ -0,0 +1,32 @@
+# サーバー証明書を定義する
+resource "aws_acm_certificate" "acm" {
+  domain_name               = "*.${aws_route53_zone.sub_domain.name}"   # サブドメインに対するワイルドカード証明書とする
+  subject_alternative_names = []                                        # 代替ドメインは無し。ワイルドカードにするので
+  validation_method         = "DNS"                                     # 検証方法はDNS/EMAILが選べる。自動更新させるならDNS
+  lifecycle {
+    create_before_destroy = true                                        # 新しい証明書を作成してから古い証明書と差し替える
+  }
+}
+
+# DNSによる証明書の検証を実施
+resource "aws_route53_record" "validation_acm" {
+  for_each        = {
+  for dvo in aws_acm_certificate.acm.domain_validation_options : dvo.domain_name => {
+    name   = dvo.resource_record_name
+    record = dvo.resource_record_value
+    type   = dvo.resource_record_type
+  }
+  }
+  allow_overwrite = true
+  name            = each.value.name
+  type            = each.value.type
+  records         = [each.value.record]
+  zone_id         = aws_route53_zone.sub_domain.zone_id
+  ttl             = 60
+}
+
+# 検証完了まで待機
+resource "aws_acm_certificate_validation" "wait_validation" {
+  certificate_arn         = aws_acm_certificate.acm.arn
+  validation_record_fqdns = [for record in aws_route53_record.validation_acm :record.fqdn]
+}

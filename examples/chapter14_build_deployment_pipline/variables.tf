71752fb4d24898ac4810a7aa61ecd2e26f966c65
Kazuhiko Okumura <engkokmr@gmail.com>
2021-11-13 11:44:43 +0900

    Chapter14 デプロイメントパイプライン
    Codepipeline を定義
    
    - SourceステージをGitHub, BuildステージをCodebuildプロジェクト, DeployステージをECSで実施する
    - GitHubと連携してリポジトリにプッシュしたらCodepipelineが走るようにWebhookを設定
    - Codepipelineのステージ間のArtifactを受け渡すためのS3バケットを定義
    - GitHubにサンプルを用意していないのでリソース作成やpipeline実行は未実施

--- examples/chapter14_build_deployment_pipline/variables.tf
+++ examples/chapter14_build_deployment_pipline/variables.tf
@@ -1,9 +1,6 @@
 variable "host_zone_name" {}
 variable "bucket_common_name" {}
 variable "container_definitions_path" {}
-variable "batch_container_definitions_path" {}
-variable "db_username" {}
-variable "db_initial_password" {}
 variable "github_name" {}
 variable "github_repo" {}
 variable "github_branch" {}
